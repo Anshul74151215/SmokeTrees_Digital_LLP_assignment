c8ac93a68e058ae2c0434915a649c119e92d98ba
Anshul ojha <ojhaanshul889@gmail.com>
2023-09-04 14:28:21 +0530

    second commit

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(
-  "mongodb+srv://Anshul_ojha:Luhsna@atlascluster.ekt7t1o.mongodb.net/assignment?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(bodyParser.json());
 
